e02f197286da0952c3b041d1d4f2bb400cb17062
HernanRossi <hfdrossi@gmail.com>
2024-04-21 20:44:43 -0700

    Add first commit to repo

---
+++ longest_substring.py
@@ -0,0 +1,48 @@
+import tracemalloc
+
+class Solution:
+
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        short_s = min([str1, str2], key=len)
+        long_s = str1 if short_s == str2 else str2
+        if str1 == str2:
+            return str1
+        div_result = self.recursive_split(list(short_s), list(long_s), 1)
+        print(f'{div_result=}')
+        if not div_result:
+            return ''
+        return ''.join(list(short_s)[:div_result])
+
+    def recursive_split(self, s_list, l_list, div_n: int) -> int:
+        slicer = int(len(s_list) / div_n)
+        sub_str = s_list[:slicer]
+        long_check = l_list[:len(sub_str)]
+        if not long_check:
+            return 0
+        if sub_str == long_check:
+            if Solution.check_full(l_list, long_check) and Solution.check_full(s_list, sub_str):
+                return slicer
+        return self.recursive_split(s_list, l_list, div_n + 1)
+
+    @staticmethod
+    def check_full(full_s, sub_s):
+        divisor = len(full_s) / len(sub_s)
+        if len(full_s) % int(divisor) == 0:
+            full_concat = sub_s * int(divisor)
+            if full_concat == full_s:
+                return True
+        return False
+
+def main():
+    tracemalloc.start()
+    instance = Solution()
+    instance.gcdOfStrings("ABABAB", "ABAB")
+    print(tracemalloc.get_traced_memory())
+    tracemalloc.stop()
+    # instance.gcdOfStrings("ABCABC", "ABC")
+    # instance.gcdOfStrings("ABCDEF", "ABC")
+    # instance.gcdOfStrings("AAAAAAAAA", "AAACCC")
+
+
+if __name__ == "__main__":
+    main()
---
+++ small_examples.py
@@ -0,0 +1,19 @@
+from typing import List
+
+def getMaxAdditionalDinersCount(N: int, K: int, M: int, S: List[int]) -> int:
+  new_guests = 0
+  count_between_guest = 0
+  S.append(N + 1)
+  seats = sorted(S)
+  prev_seat = 0
+  for seat in seats:
+    available_seats = seat - prev_seat - 1
+    prev_seat = seat
+    for i in range(available_seats):
+      if available_seats >= K + K + 1:
+        new_guests +=1
+        available_seats -= 1
+        i += K
+      else:
+        break
+  return new_guests
