9e2f60d0be8b9ab175f5907778c745e28f84bad1
HernanRossi <hfdrossi@gmail.com>
2025-01-26 14:19:06 -0800

    Add Find Anagrams

--- find_anagrams.py
+++ find_anagrams.py
@@ -1,24 +1,22 @@
 import string
 from collections import defaultdict
+from dataclasses import dataclass
 from typing import List, Dict
 
 # A map of the letter in the alphabet to its position in our 25 length list
 CHAR_MAP = {c: i for (i, c) in enumerate(list(string.ascii_lowercase))}
 
 
+@dataclass
+class AnagramGroup:
+    anagrams: List[str]
+
+
 class Solution:
     """sub_list_map: is a dict {index_in_original_list: [count_map]}"""
     _input_list: List[str] = []
 
-    def remove_redundant_brackets(self, nested_list):
-        if isinstance(nested_list, list) and len(nested_list) == 1 and isinstance(nested_list[0], list):
-            return self.remove_redundant_brackets(nested_list[0])
-        elif isinstance(nested_list, list):
-            return [self.remove_redundant_brackets(item) for item in nested_list]
-        else:
-            return nested_list
-
-    def recursive_check(self, index_list_map: Dict[int, List[int]], char_index: int) -> List[str]:
+    def recursive_check(self, index_list_map: Dict[int, List[Dict]], char_index: int) -> list[AnagramGroup]:
         """
         Recurse through each list in the index_list_map and split the list up into
         the lists that match on the current letter at char_index
@@ -29,7 +27,7 @@ class Solution:
         if len(index_list_map) == 1:
             # we are at the base case for a single list with no matches
             final_return = self._input_list[list(index_list_map.keys())[0]]
-            return [final_return]
+            return [AnagramGroup([final_return])]
         # map the count for the current character to each string that matches on that character count
         matching_index_list_map: Dict[int, Dict[int, List[dict]]] = defaultdict(dict)
         # go through each list and get count for the current letter char_index
@@ -37,11 +35,14 @@ class Solution:
             matching_index_list_map[value[char_index]].update({key: value})
         # we have reached the count for 'z' if it is equal return all the lists else one more recursion
         if char_index == 24:
-            for matchs in matching_index_list_map.values():
-                final_matches = [self._input_list[s_ind] for s_ind in matchs.keys()]
-                print(f'Returning final matches {final_matches}')
-                return final_matches
-        return [self.recursive_check(sub_map, char_index + 1) for sub_map in matching_index_list_map.values()]
+            for matches in matching_index_list_map.values():
+                final_matches = [self._input_list[s_ind] for s_ind in matches.keys()]
+                anagram_group = AnagramGroup(final_matches)
+                return [anagram_group]
+        result = []
+        for sub_map in matching_index_list_map.values():
+            result.extend(self.recursive_check(sub_map, char_index + 1))
+        return result
 
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         self._input_list = strs
@@ -50,12 +51,12 @@ class Solution:
         for idx, c_string in enumerate(strs):
             # this is a 25 length list for each letter in the alphabet initialize the char count list to 0
             index_list_map[idx] = [0 for _ in range(25)]
+            count_list: List[int] = []
             for char in c_string:
                 index_list_map[idx][CHAR_MAP[char]] = index_list_map[idx][CHAR_MAP[char]] + 1
             # we now have a dict from index in input string to list of char counts
-        # print(f'Created the Character count grid {index_list_map}')
-        final_result = self.remove_redundant_brackets([self.recursive_check(index_list_map, 0)])
-        print(f'The final result is {final_result}')
+        anagram_groups = self.recursive_check(index_list_map, 0)
+        final_result = [group.anagrams for group in anagram_groups]
         return final_result
 
     # def groupAnagramsOld(self, strs: List[str]) -> List[List[str]]:
